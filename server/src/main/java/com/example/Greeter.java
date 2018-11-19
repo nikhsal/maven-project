c9757d2fda93363f628dc6156a32e5d50f646008
nikhsal <44611667+nikhsal@users.noreply.github.com>
2018-11-19 18:47:07 +0530

    updated

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,7 @@ public class Greeter {
 
   }
   /**
-  *@param name of someone
+  *@param someone name of a person
   *@return greeting string
   */
   public final String greet(final String someone) {
